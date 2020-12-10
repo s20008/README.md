6b118bfce56b334fdfefdb477b62c1f435b774d9
vagrant <vagrant@ubuntu2004.localdomain>
2020-12-10 08:04:10 +0000

    MoneyBook

--- src/App.js
+++ src/App.js
@@ -3,14 +3,45 @@ import './App.css'
 const MoneyBook = () => {
   return (
     <div>
-      <h1>小遣い帳</h1>
-  <table>
-  <thead></thead>
-  <tbody></tbody>
-  </table>
+    <h1>小遣い帳</h1>
+    <table className="book">
+      <thead>
+        <tr>
+          <th>日付</th>
+          <th>項目</th>
+          <th>入金</th>
+          <th>出金</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1/1</td>
+          <td>お年玉</td>
+          <td>1000</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>1/3</td>
+          <td>ケーキ</td>
+          <td></td>
+          <td>500</td>
+        </tr>
+        <tr>
+          <td>2/1</td>
+          <td>小遣い</td>
+          <td>3000</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>2/5</td>
+          <td>マンガ</td>
+          <td></td>
+          <td>600</td>
+        </tr>
+      </tbody>
+    </table>
     </div>
   )
-
 }
 
 export default MoneyBook 
